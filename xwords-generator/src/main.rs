464d25bd6009afcfc2f0be9dc47d7c3996719d4d
cstafie <stafie.cristian@gmail.com>
2022-09-24 16:19:46 -0700

    read csv in rust

--- xwords-generator/src/main.rs
+++ xwords-generator/src/main.rs
@@ -1,3 +1,19 @@
+use csv;
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = "../xwords_data/2017.csv";
+    let mut results = csv::Reader::from_path(file_path).unwrap();
+    let mut five_letter_words = vec![];
+
+    for result in results.records() {
+        let record = result.expect("a CSV record");
+        let word = record[2].to_string();
+
+        if word.len() == 5 && !word.contains(" ") {
+            println!("{:?}", word);
+            five_letter_words.push(word);
+        }
+    }
+
+    return ();
 }
