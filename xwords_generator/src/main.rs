731f8f042377632197b29cb84dd68ae357ca2fda
cristian <stafie.cristian@gmail.com>
2022-10-08 23:14:42 -0700

    trying to get xword entries to work wip

--- xwords_generator/src/main.rs
+++ xwords_generator/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     cmp,
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     str,
 };
 
@@ -34,27 +34,39 @@ impl XWord {
         }
     }
 
-    pub fn get_across(&self, row: usize, col: usize) -> String {
-        (col..self.width)
+    pub fn get_entry(&self, entry: &XWordEntry) -> String {
+        match entry.direction {
+            Direction::Across => self.get_across(entry.row, entry.col, entry.length),
+            Direction::Down => self.get_down(entry.row, entry.col, entry.length),
+        }
+    }
+    fn get_across(&self, row: usize, col: usize, length: usize) -> String {
+        (col..col + length)
             .map(|i| self.grid[row][i])
             .collect::<String>()
             .trim()
             .to_string()
     }
-    pub fn set_across(&mut self, row: usize, col: usize, word: &str) {
-        for (i, char) in word.chars().enumerate() {
-            self.grid[row][col + i] = char;
-        }
-    }
-
-    pub fn get_down(&self, row: usize, col: usize) -> String {
-        (row..self.height)
+    fn get_down(&self, row: usize, col: usize, length: usize) -> String {
+        (row..row + length)
             .map(|i| self.grid[i][col])
             .collect::<String>()
             .trim()
             .to_string()
     }
-    pub fn set_down(&mut self, row: usize, col: usize, word: &str) {
+
+    pub fn set_entry(&mut self, entry: &XWordEntry, s: &str) {
+        match entry.direction {
+            Direction::Across => self.set_across(entry.row, entry.col, s),
+            Direction::Down => self.set_down(entry.row, entry.col, s),
+        }
+    }
+    fn set_across(&mut self, row: usize, col: usize, word: &str) {
+        for (i, char) in word.chars().enumerate() {
+            self.grid[row][col + i] = char;
+        }
+    }
+    fn set_down(&mut self, row: usize, col: usize, word: &str) {
         for (i, char) in word.chars().enumerate() {
             self.grid[row + i][col] = char
         }
@@ -135,70 +147,70 @@ fn get_matching_words<'a>(words_vec: &'a Vec<String>, entry: &String) -> &'a [St
     };
 }
 
-fn insert_vertical(
-    xword: &mut XWord,
-    col: usize,
-    words_vec: &Vec<String>,
-    used_words: &mut HashSet<String>,
-) {
-    let entry = &xword.get_down(0, col);
-    let words = get_matching_words(words_vec, entry);
+// fn insert_vertical(
+//     xword: &mut XWord,
+//     col: usize,
+//     words_vec: &Vec<String>,
+//     used_words: &mut HashSet<String>,
+// ) {
+//     let entry = &xword.get_down(0, col);
+//     let words = get_matching_words(words_vec, entry);
 
-    if col == xword.width - 1 && !used_words.contains(entry) {
-        if let Ok(_i) = words_vec.binary_search(entry) {
-            // we found a solution!
-            println!("{:?}", xword);
-            return;
-        }
-    }
+//     if col == xword.width - 1 && !used_words.contains(entry) {
+//         if let Ok(_i) = words_vec.binary_search(entry) {
+//             // we found a solution!
+//             println!("{:?}", xword);
+//             return;
+//         }
+//     }
 
-    for word in words {
-        if used_words.contains(word) {
-            continue;
-        }
+//     for word in words {
+//         if used_words.contains(word) {
+//             continue;
+//         }
 
-        let used_word: String = word.chars().collect();
-        xword.set_down(0, col, &used_word);
-        used_words.insert(used_word);
+//         let used_word: String = word.chars().collect();
+//         xword.set_down(0, col, &used_word);
+//         used_words.insert(used_word);
 
-        // move to the next iteration
-        insert_horizontal(xword, col + 1, words_vec, used_words);
+//         // move to the next iteration
+//         insert_horizontal(xword, col + 1, words_vec, used_words);
 
-        // reset the xword
-        xword.set_down(0, col, &format!("{:height$}", entry, height = xword.height));
-        used_words.remove(&word.chars().collect::<String>());
-    }
-}
+//         // reset the xword
+//         xword.set_down(0, col, &format!("{:height$}", entry, height = xword.height));
+//         used_words.remove(&word.chars().collect::<String>());
+//     }
+// }
 
-fn insert_horizontal(
-    xword: &mut XWord,
-    row: usize,
-    words_vec: &Vec<String>,
-    used_words: &mut HashSet<String>,
-) {
-    let entry = &xword.get_across(row, 0);
-    let words = get_matching_words(words_vec, entry);
+// fn insert_horizontal(
+//     xword: &mut XWord,
+//     row: usize,
+//     words_vec: &Vec<String>,
+//     used_words: &mut HashSet<String>,
+// ) {
+//     let entry = &xword.get_across(row, 0);
+//     let words = get_matching_words(words_vec, entry);
 
-    for (i, word) in words.iter().enumerate() {
-        if used_words.contains(word) {
-            continue;
-        }
+//     for (i, word) in words.iter().enumerate() {
+//         if used_words.contains(word) {
+//             continue;
+//         }
 
-        if row == 0 {
-            println!("{}% {}", i * 100 / words.len(), word);
-        }
+//         if row == 0 {
+//             println!("{}% {}", i * 100 / words.len(), word);
+//         }
 
-        let used_word: String = word.chars().collect();
-        xword.set_across(row, 0, &used_word);
-        used_words.insert(used_word);
+//         let used_word: String = word.chars().collect();
+//         xword.set_across(row, 0, &used_word);
+//         used_words.insert(used_word);
 
-        insert_vertical(xword, row, words_vec, used_words);
+//         insert_vertical(xword, row, words_vec, used_words);
 
-        // reset the xword
-        xword.set_across(row, 0, &format!("{:width$}", entry, width = xword.width));
-        used_words.remove(&word.chars().collect::<String>());
-    }
-}
+//         // reset the xword
+//         xword.set_across(row, 0, &format!("{:width$}", entry, width = xword.width));
+//         used_words.remove(&word.chars().collect::<String>());
+//     }
+// }
 
 fn get_xword_entries_across(xword: &XWord, entries: &mut Vec<XWordEntry>, row: usize) {
     // last row is xword.height - 1
@@ -239,21 +251,54 @@ fn get_xword_entries(xword: &XWord) -> Vec<XWordEntry> {
     entries
 }
 
-fn main() {
-    let height = 3;
-    let width = 3;
+fn generate_xwords(
+    xword: &mut XWord,
+    entries: &mut VecDeque<XWordEntry>,
+    words_map: &HashMap<usize, Vec<String>>,
+    used_words: &mut HashSet<String>,
+) {
+    if entries.is_empty() {
+        // we found a solution?
+        println!("{:?}", xword);
+        return;
+    }
 
-    let words = get_words(1, 300);
+    let entry = entries.pop_front().unwrap();
+    let old_entry_string = xword.get_entry(&entry);
+    let words_vec = words_map.get(&entry.length).unwrap();
+    let matching_words = get_matching_words(words_vec, &old_entry_string);
+
+    for word in matching_words {
+        if used_words.contains(word) {
+            continue;
+        }
+
+        let used_word = word.clone();
+        used_words.insert(used_word);
+
+        xword.set_entry(&entry, word);
+
+        generate_xwords(xword, entries, words_map, used_words);
+
+        xword.set_entry(&entry, &old_entry_string);
+        used_words.remove(word);
+    }
+}
+
+fn main() {
+    let height = 5;
+    let width = 5;
+
+    let words_map = get_words(1, 300);
 
     // TODO: the empty crossword should be given some kind of grid, for now it's just a width and height
     let mut xword = XWord::new(width, height);
 
-    // generate a list of the words we need:
-    // [0,0] needs an across word of length 3, [0,4] needs an across word of length 5
-
-    let entries = get_xword_entries(&xword);
+    let mut entries = VecDeque::from(get_xword_entries(&xword));
 
     println!("{:?}", entries);
+
+    generate_xwords(&mut xword, &mut entries, &words_map, &mut HashSet::new());
 
     // insert_horizontal(
     //     &mut xword,
