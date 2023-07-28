d0b1ce58367cfbb40613c9b6d6c3fb87d84b6128
cstafie <stafie.cristian@gmail.com>
2023-07-27 23:27:47 -0700

    trying adsterra ads

--- nx/apps/client/src/screens/Game/XWord/index.tsx
+++ nx/apps/client/src/screens/Game/XWord/index.tsx
@@ -73,22 +73,25 @@ const XWord = ({ game }: Props) => {
 
   return (
     <>
-      {isGameOver && (
+      {!isGameOver && (
         <section className="flex flex-col gap-8 justify-center items-center">
+          {/* <script type="text/javascript">
+            {
+              null && ({const atOptions = {
+                key: '9e8938cf20423570703dcc63d62e8746',
+                format: 'iframe',
+                height: 60,
+                width: 468,
+                params: {},
+              }})
+            }
+          </script>
+
           <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1932049158468894"
-            crossOrigin="anonymous"
-          ></script>
-          <ins
-            className="adsbygoogle"
-            style={{ display: 'block' }}
-            data-ad-client="ca-pub-1932049158468894"
-            data-ad-slot="3023427890"
-            data-ad-format="auto"
-            data-full-width-responsive="true"
-          ></ins>
-          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+            type="text/javascript"
+            src="https://www.profitabledisplaynetwork.com/9e8938cf20423570703dcc63d62e8746/invoke.js"
+          ></script> */}
+
           <button onClick={createGame} className="btn btn-blue">
             START A NEW GAME!
           </button>
