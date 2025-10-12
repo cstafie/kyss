d673fb2ffbae1b11d6e31b3f1e33e42edb3a69a0
Cristian Stafie <stafie.cristian@gmail.com>
2025-10-12 11:00:34 -0700

    fixing frontend lint and build issues and cleaning up backend .gitignore stuff

--- crossable/apps/backend/dist/controllers/entity/entity.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Entity {
-    constructor(id) {
-        this.id = id ? id : crypto.randomUUID();
-        this.createdAt = new Date();
-    }
-}
-exports.default = Entity;
